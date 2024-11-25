5ddb22c0786a199eb05881671eee792a54041a27
Immanuel-Aristotle <Cicero.Elead.Apollonius@gmail.com>
2024-11-25 18:10:12 +0800

    modify vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? 'https://immanuel-aristotle.github.io/DI-scoring-program.github.io/' : '/'
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "/DI-scoring-program.github.io/"
+      : "/"
 };
