7e1baf40a92301fa0e5d5dd86ea9812038681d33
Immanuel-Aristotle <Cicero.Elead.Apollonius@gmail.com>
2024-11-25 18:50:47 +0800

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
-// vue.config.js
 module.exports = {
-  publicPath: '' // This removes the base path, so assets will be referenced without any prefix
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "/DI-scoring-program/"
+      : "/"
 };
